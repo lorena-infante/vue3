9a4d789b9068c5413277dca4cec99801aeb179f3
Lorena Infante <m.lorenainfanter@gmail.com>
2024-05-28 23:01:27 -0400

    feat: :books: adding a watcher for the event guardarLS when carrito's state changes

--- guitarla-vue/src/App.vue
+++ guitarla-vue/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, onMounted } from 'vue';
+import { ref, reactive, onMounted, watch } from 'vue';
 import { db } from './data/guitarras';
 import Guitarra from './components/Guitarra.vue';
 import Header from './components/Header.vue';
@@ -8,6 +8,10 @@ import Footer from './components/Footer.vue';
 const guitarras = ref([]);
 const carrito = ref([]);
 const guitarra = ref([]);
+
+watch(carrito, () => { guardarLS() },
+    { deep: true }
+);
 
 onMounted(() => {
     guitarras.value = db;
