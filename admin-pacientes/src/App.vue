cd87e0cc88d06d65277f72395937441564ade04b
Lorena Infante <m.lorenainfanter@gmail.com>
2024-06-18 20:23:04 -0400

    feat: :sparkles: adding tailwind (finally!)

---
+++ admin-pacientes/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class="container mx-auto mt-20">
+    <Header />
+  </div>
+</template>
---
+++ admin-pacientes/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="font-black text-3xl text-center">
+        Seguimiento Pacientes
+        <span class="text-indigo-600">
+            Veterinaria
+        </span>
+    </h1>
+</template>
